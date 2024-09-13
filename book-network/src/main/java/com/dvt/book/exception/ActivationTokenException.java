984d876464fc9a3fe71cd9a21fa78c2e16156e18
vnthiendang <thien25078@gmail.com>
2024-09-13 23:40:23 +0700

    handle exceptions

---
+++ book-network/src/main/java/com/dvt/book/exception/ActivationTokenException.java
@@ -0,0 +1,7 @@
+package com.dvt.book.exception;
+
+public class ActivationTokenException extends RuntimeException{
+    public ActivationTokenException(String message){
+        super(message);
+    }
+}
