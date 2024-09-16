f321eee94059c277b74d644a9852ec84179e5ad0
vnthiendang <thien25078@gmail.com>
2024-09-16 15:54:08 +0700

    init fe source

--- book-network/src/main/java/com/dvt/book/exception/ActivationTokenException.java
+++
@@ -1,7 +0,0 @@
-package com.dvt.book.exception;
-
-public class ActivationTokenException extends RuntimeException{
-    public ActivationTokenException(String message){
-        super(message);
-    }
-}
